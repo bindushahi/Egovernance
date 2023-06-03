1ae35417b1999d6db87efc1a68ad647651dc5b90
Bindu Shahi Thakuri <bindushahi40@gmail.com>
2023-06-03 18:23:51 +0545

    weather

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
-// Get Weather Alerts button click handler
-function handleGetWeatherAlerts() {
-  // Replace this with your desired functionality
-  console.log("Get Weather Alerts clicked");
-}
 function goToDriverFeedbackForm(){
   window.location.href = "driver_feedback_form.html";
 }
 function goToAccidentReportForm(){
   window.location.href = "accident_report_form.html";
+}
+function ShowWeatherAlert()
+{
+  window.location.href = "weather.html";
+
 }
