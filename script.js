60dcaf724a456adf7b93ac6192bcedc6d5138cb2
Bindu Shahi Thakuri <bindushahi40@gmail.com>
2023-06-03 18:17:52 +0545

    change

--- script.js
+++ script.js
@@ -1,59 +1,8 @@
-// Report Accident button click handler
-function handleReportAccident() {
-  // Replace this with your desired functionality
-  console.log("Report Accident clicked");
-}
-
-// Emergency Assistance button click handler
-function handleEmergencyAssistance() {
-  // Replace this with your desired functionality
-  console.log("Emergency Assistance clicked");
-}
-
-// Submit Feedback button click handler
-function handleSubmitFeedback() {
-  // Replace this with your desired functionality
-  console.log("Submit Feedback clicked");
-}
 
 // Get Weather Alerts button click handler
 function handleGetWeatherAlerts() {
   // Replace this with your desired functionality
   console.log("Get Weather Alerts clicked");
-}
-
-// Attach event listeners to the buttons
-document.addEventListener("DOMContentLoaded", function() {
-  var reportAccidentButton = document.querySelector("#report-accident-btn");
-  reportAccidentButton.addEventListener("click", handleReportAccident);
-
-  var emergencyAssistanceButton = document.querySelector("#emergency-assistance-btn");
-  emergencyAssistanceButton.addEventListener("click", handleEmergencyAssistance);
-
-  var submitFeedbackButton = document.querySelector("#submit-feedback-btn");
-  submitFeedbackButton.addEventListener("click", handleSubmitFeedback);
-
-  var getWeatherAlertsButton = document.querySelector("#get-weather-alerts-btn");
-  getWeatherAlertsButton.addEventListener("click", handleGetWeatherAlerts);
-});
-// Function to handle reporting an accident
-function handleReportAccident() {
-  alert("Accident reported successfully!");
-}
-
-// Function to handle emergency assistance
-function handleEmergencyAssistance() {
-  alert("Emergency assistance requested!");
-}
-
-// Function to handle submitting driver feedback
-function handleSubmitFeedback() {
-  alert("Driver feedback submitted!");
-}
-
-// Function to handle getting weather alerts
-function handleGetWeatherAlerts() {
-  alert("Weather alerts retrieved!");
 }
 function goToDriverFeedbackForm(){
   window.location.href = "driver_feedback_form.html";
