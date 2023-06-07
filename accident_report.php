a3e7a65451ff33afc62f1465d9930e1da9b6d8d8
ayushmakafle <amikafle1@gmail.com>
2023-06-07 18:24:44 +0545

    accident report form backend

--- accident_report.php
+++ accident_report.php
@@ -1,29 +1,28 @@
 <?php
 // Replace DB_HOST, DB_USER, DB_PASS, and DB_NAME with your database credentials
-$conn = mysqli_connect("DB_HOST", "DB_USER", "DB_PASS", "DB_NAME");
+$conn = mysqli_connect("localhost", "root", "", "road safety monitoring system");
 
 // Get the form data
-$datetime = $_POST['reportDateTime'];
-$location = $_POST['location'];
-$description = $_POST['description'];
-$vehicleLicencePlate = $_POST['vehicleLicencePlate'];
-$vehicletype = $_POST['vehicleType'];
-$vehiclecolor=$_POST['vehicleColor'];
-$drivername=$_POST['driverName'];
-$drivercontact=$_POST['driverContact'];
-$driverlicence=$_POST['driverLicence'];
-$witnessname=$_POST['witnessName'];
-$witnesscontact=$_POST['witnessContact'];
-$injuries=$_POST['injuries'];
-$damages=$_POST['damages'];
-$photos =$_POST['photos'];
-$policeReportNumber=$_POST['policeReportNumber'];
-$additionalcomments=$_POST['additionalComments'];
+$datetime = $_POST['ReportDateTime'];
+$location = $_POST['Location'];
+$description = $_POST['Description'];
+$vehicleLicencePlate = $_POST['VehicleLicensePlate'];
+$vehicletype = $_POST['VehicleType'];
+$vehiclecolor=$_POST['VehicleColor'];
+$drivername=$_POST['DriverName'];
+$drivercontact=$_POST['DriverContact'];
+$driverlicence=$_POST['DriverLicense'];
+$witnessname=$_POST['WitnessName'];
+$witnesscontact=$_POST['WitnessContact'];
+$injuries=$_POST['Injuries'];
+$damages=$_POST['Damages'];
+$photos =$_FILES['Photos'];
+$additionalcomments=$_POST['AdditionalComments'];
 
 // Insert data into the bill table
-$query = "INSERT INTO accidentreports (ReportDateTime,Location,Description,VehicleLicencePlate,VehicleType,VehicleColor,DriverName,DriverContact,DriverLicense,WitnessName,WitnessContact,Injuries,Damages,Photos,AdditionalComments) 
-VALUES ('$datetime',$location', '$description', '$vehicleLicencePlate', '$vehicletype', '$vehiclecolor','$drivername',
-'$drivercontact','$driverlicence','$witnessname','$witnesscontact,'$injuries','$damages','$photos','$additionalcomments')";
+$query = "INSERT INTO accidentreports (ReportDateTime,Location,Description,VehicleLicensePlate,VehicleType,VehicleColor,DriverName,DriverContact,DriverLicense,WitnessName,WitnessContact,Injuries,Damages,Photos,AdditionalComments) 
+VALUES ('$datetime','$location', '$description', '$vehicleLicencePlate', '$vehicletype', '$vehiclecolor','$drivername',
+'$drivercontact','$driverlicence','$witnessname','$witnesscontact','$injuries','$damages','$photos','$additionalcomments')";
 $result = mysqli_query($conn, $query);
 
 if ($result) {
