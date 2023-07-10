bbf818984bd9f51d21fd6746c4aa56256252171c
ayushmakafle <amikafle1@gmail.com>
2023-07-10 18:04:02 +0545

    admin road update

--- dbconnect.php
+++ dbconnect.php
@@ -5,8 +5,7 @@ $password="";
 $dbname="road safety monitoring system";
 $conn=mysqli_connect($servername, $username, $password, $dbname);
 if(!$conn){
-    die("connection failed:". mysqli_connect_error());
+  die("connection failed:". mysqli_connect_error());
 }
-else
-echo"conneted successfully";
-?>
+//else
+//echo"conneted successfully";
